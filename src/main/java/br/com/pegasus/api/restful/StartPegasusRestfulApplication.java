7c20f743fb143d34101a48e8086730e27a6b0b7e
wpoliveiracel <wp.oliveira.cel@gmail.com>
2025-03-29 21:51:00 -0300

    criação de um projeto basico crud para possuir um contrato openOPI

--- src/main/java/br/com/pegasus/api/restful/StartPegasusRestfulApplication.java
+++ src/main/java/br/com/pegasus/api/restful/StartPegasusRestfulApplication.java
@@ -2,8 +2,10 @@ package br.com.pegasus.api.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"br.com.pegasus.api.restful", "br.com.pegasus.openapi"})
 public class StartPegasusRestfulApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/pegasus/api/restful/app/resource/ItemControllerImpl.java
@@ -0,0 +1,8 @@
+package br.com.pegasus.api.restful.app.resource;
+
+import br.com.openapi.app.controller.ItemsApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemControllerImpl implements ItemsApi {
+}
